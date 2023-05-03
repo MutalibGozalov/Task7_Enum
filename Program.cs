30add0ba14842cd4ffcb0e8515a6d94cc6a975ab
Mutalib SH. Gozalov <mutalibshg@code.local>
2023-05-03 13:03:34 +0400

    task done

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Enum;
+using Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var properties = typeof(ShippingStatus).GetFields(); 
+        foreach (var prop in properties)
+        {
+           var dd = prop.GetCustomAttribute(typeof(DisplayAttribute)) as DisplayAttribute;
+           if(dd != null){
+                Console.WriteLine(prop.Name + " --> " + dd.Name); 
+           }
+        }   
+
+        //------------[Flags]-------------//
+
+    
+      Console.WriteLine(
+           "\n\nAll possible combinations of values without FlagsAttribute:");
+      for(int val = 0; val <= 16; val++ )
+         Console.WriteLine( "{0,3} - {1:G}", val, (SingleHue)val);
+
+      //with  [Flags]
+      Console.WriteLine(
+           "\nAll possible combinations of values with FlagsAttribute:");
+      for( int val = 0; val <= 16; val++ )
+         Console.WriteLine( "{0,3} - {1:G}", val, (MultiHue)val);  
+    }
+}
